77538df3014978d18edff57bf081881b00412ece
ayebravo <67244374+ayebravo@users.noreply.github.com>
2021-04-29 15:22:14 -0500

    Got sorting working (UI)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,7 +19,10 @@ export class HomeComponent implements OnInit {
   students: Student[] = [];
 
   ngOnInit(): void {
-    this.students = this.studentDataSvc.getStudents();
+    this.students = this.studentDataSvc
+      .getStudents()
+      .sort((a, b) => a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1)
+      ;
   }
 
   addStudent() {
